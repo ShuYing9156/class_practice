6385a8a4abb2f3540d56f378648acd3a50d75a9e
ruby <ruby2015095231128@gmail.com>
2024-01-04 14:13:54 +0800

    測試termcolor的用法

--- wordle.py
+++ wordle.py
@@ -1,7 +1,12 @@
-print ('hello world')
-print (123456789)
+#print ('hello world')
+#print (123456789)
 
+import sys
+from termcolor import colored, cprint
+ 
 
+a = 'Hello, World!'
+cprint(a, 'white', 'on_grey', ['concealed'])
 
 
 
